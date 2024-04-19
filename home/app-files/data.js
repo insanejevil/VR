92443bd0989c405f0f6bc2f9fd8dd7fd53dc59e5
insanejevil <160987521+insanejevil@users.noreply.github.com>
2024-04-19 11:58:07 +0800

    1

---
+++ home/app-files/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--1",
+      "name": "龍山寺-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1408,
+      "initialViewParameters": {
+        "yaw": 3.1116678002239,
+        "pitch": -0.4738548066411816,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.8986497895418255,
+          "pitch": -0.025981043370283174,
+          "title": "龍山寺",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
