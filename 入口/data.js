69c505626974fea0346e32bfe8f8e3773f3255d9
insanejevil <160987521+insanejevil@users.noreply.github.com>
2024-04-19 11:46:42 +0800

    1

---
+++ 入口/data.js
@@ -0,0 +1,252 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--1",
+      "name": "龍山寺入口-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.5576463150279682,
+        "pitch": -0.48536400732056784,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.44637617332783464,
+          "pitch": -0.34580965689258036,
+          "rotation": 0,
+          "target": "1--1"
+        },
+        {
+          "yaw": 0.46255612917499533,
+          "pitch": -0.13019510042405003,
+          "rotation": 0,
+          "target": "2--1"
+        },
+        {
+          "yaw": 0.46738443851037204,
+          "pitch": 0.03066737449944945,
+          "rotation": 0,
+          "target": "3--1"
+        },
+        {
+          "yaw": 0.4800902562643632,
+          "pitch": 0.14997697822941802,
+          "rotation": 0,
+          "target": "4--1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7967953974121862,
+          "pitch": -0.3750153519011299,
+          "title": "靜心瀑布",
+          "text": "於1996年完工的淨心瀑布，其名取自佛教《大智度經論》「人本無相，相由心生」，並寄託「瀧水奔騰，飛泉澈清，朝登佛門，先淨其心，心靈淨化，悟道慧根，具足相好，莊嚴其身」的寓意。"
+        }
+      ]
+    },
+    {
+      "id": "1--1",
+      "name": "龍山寺正殿-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1300,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1056676943832358,
+          "pitch": -0.5208816165660188,
+          "rotation": 0,
+          "target": "0--1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.387832106691187,
+          "pitch": -0.6612345414935259,
+          "title": "Title",
+          "text": "<div>交趾陶</div><div>交趾陶又稱交趾燒、交趾尪仔或瓷尪仔，為建築上裝飾性的低溫彩釉軟陶，通常以民間故事、神話傳說等題材為主，裝飾於屋頂、柱樑、水車堵、墀、牆等位置。臺灣交趾分為兩大系統，一出於廣東葉王，一傳承自福建廈門柯訓。</div>"
+        }
+      ]
+    },
+    {
+      "id": "2--1",
+      "name": "龍山寺正殿之二-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4267821489957644,
+          "pitch": 0.0758756817009889,
+          "rotation": 0,
+          "target": "0--1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5659359099932555,
+          "pitch": -0.7215572176561071,
+          "title": "Title",
+          "text": "<div>傳統社會中，寺廟不僅是地方信仰中心，也是經濟、文化與藝術的表現。為了表示對重要寺廟的重視，顯貴及文人經常贈予匾額，懸於殿堂內。包括清光緒皇帝「慈輝遠蔭」（現存為複製品）、艋舺商業組織泉郊「佛法皈依」、于右任所題「光明淨域」等，為臺灣歷史發展見證。</div><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "3--1",
+      "name": "龍山寺正殿之三-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0915727455376985,
+          "pitch": 0.02762557927821696,
+          "rotation": 0,
+          "target": "0--1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6428214326134771,
+          "pitch": -0.6390325933885705,
+          "title": "護國祐民",
+          "text": "民國六十八年竹林山觀音寺贈。"
+        },
+        {
+          "yaw": 0.5013322257564266,
+          "pitch": -0.6249750857393241,
+          "title": "慈雲普照",
+          "text": "民國五十二年由龍山寺董監事會所立。"
+        }
+      ]
+    },
+    {
+      "id": "4--1",
+      "name": "龍山寺正殿之五-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1408,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7922542700023794,
+          "pitch": -0.18444667204958165,
+          "rotation": 0,
+          "target": "0--1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.289513896066108,
+          "pitch": -0.3351929392559203,
+          "title": "晴空小劫 道託踈狂",
+          "text": "後殿左翼門柱上，可見清末大書法家吳昌碩（1844-1927）的石鼓文字跡。「壬戌（1922）清明節」、「情空小劫，道託疏狂。」和「七十六歲老人吳昌碩」題款。典故來自石濤（1642-1707）畫中詩：「忘情空小劫，有道託疏狂。」情空，放空性情；一增一減間，曰：「一小劫」；道託，即「導託」是寄託、引領之意；疏狂，恣情享受、狂放不羈。這兩小句是說：「漫漫長長的閒暇時間裡，總希望能有一種寄託與擁有，於是再也不想約束自身，就縱情的享受人生吧！」禪意八字，道出老翁的勸勉。"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
