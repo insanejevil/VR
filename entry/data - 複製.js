0a39d6a209d4ca22de6a4d1e232a1502afecced4
insanejevil <160987521+insanejevil@users.noreply.github.com>
2024-04-26 11:37:10 +0800

    1

--- entry/data - 複製.js
+++ entry/data - 複製.js
@@ -246,7 +246,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": true,
-    "viewControlButtons": true
+    "fullscreenButton": false,
+    "viewControlButtons": false
   }
 };
